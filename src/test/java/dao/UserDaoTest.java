fc48c61f24ff27f6beb176f547e3ad9e0ce652d3
qooa <goodMorning@abc.com>
2021-06-13 22:37:41 +0800

    second commit

--- src/test/java/dao/UserDaoTest.java
+++ src/test/java/dao/UserDaoTest.java
@@ -27,14 +27,14 @@ public class UserDaoTest
     @Test
     public void updateUser()
     {
-        User user=new User(3,"tom123","12367");
+        User user = new User(3, "tom123", "12367");
         System.out.println(userDao.updateUser(user));
     }
 
     @Test
     public void addUser()
     {
-        System.out.println(userDao.addUser(new User(null,"tom1","123")));
+        System.out.println(userDao.addUser(new User(null, "tom1", "123")));
     }
 
     @Test
@@ -52,7 +52,7 @@ public class UserDaoTest
     @Test
     public void getUserByUsernameAndPassword()
     {
-        System.out.println(userDao.getUserByUsernameAndPassword("tom","1234or1=1"));
+        System.out.println(userDao.getUserByUsernameAndPassword("tom", "1234or1=1"));
     }
 
     @Test
@@ -65,5 +65,7 @@ public class UserDaoTest
     public void getUserIdByName()
     {
         System.out.println(userDao.getUserIdByName("tom1"));
+        System.out.println("git");
+
     }
 }
